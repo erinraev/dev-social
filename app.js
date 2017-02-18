9f4561160c1acd13785ad212f5ca75edc520e6e5
Erin <vaagee1@gmail.com>
2017-02-18 14:19:06 -0700

    nope

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-angular.module('socialApp', []);
+angular.module('socialApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('welcome', {
+          url: '/welcome',
+          templateUrl: './views/initial.html'
+      })
+
+      .state('landing', {
+       url: '/landing',
+       templateUrl: 'views/landing.html'
+     })
+
+     .state('update', {
+        url: '/update',
+        templateUrl: 'update.html'
+      })
+
+      .state('view', {
+       url: '/view',
+       templateUrl: 'viewfriends.html'
+     })
+
+     .state('find', {
+      url: '/find',
+      templateUrl: 'findfriends.html'
+    })
+
+     .state('info', {
+       url: '/info',
+       templateUrl: 'friendinfo.html'
+     })
+
+    $urlRouterProvider
+      .otherwise('/welcome')
+
+ });
